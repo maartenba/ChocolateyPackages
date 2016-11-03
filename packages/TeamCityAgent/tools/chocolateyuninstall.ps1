205fc578ed5e4915f7831f785142e136c550dcf9
Ethan Spoelstra <espoelstragithub@deik.me>
2016-11-03 16:13:11 -0500

    Added uninstall, refactored to use Install-ChocolateyZipPackage

---
+++ packages/TeamCityAgent/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'TeamCityAgent'
+$zipFileName = 'buildAgent.zip'
+
+$uninstalled = $false
+Uninstall-ChocolateyZipPackage -PackageName $packageName `
+                                -ZipFileName  $zipFileName
+$uninstalled = $true
